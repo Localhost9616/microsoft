cda19ed08392c88b9bf6f51840f210af43b1afde
Localhost9616 <87538453+Localhost9616@users.noreply.github.com>
2022-03-02 20:52:29 +0530

    Microsoft Clone

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    screens: {
+      xsm: '0px',
+      vsm: '380px',
+      sm: '539px',
+      md: '768px',
+      amd: '857px',
+      lg: '1083px',
+      xl: '1440px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
